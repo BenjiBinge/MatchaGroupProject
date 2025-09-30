37809adc6a7c4ae1e3ee0c381ba2e20f5848abc7
westenravn-boop <westenravn@gmail.com>
2025-09-30 11:08:38 +0200

    Enemy changes

--- Assets/Scripts/Enemy/FollowEnemy.cs
+++ Assets/Scripts/Enemy/FollowEnemy.cs
@@ -1,58 +1,99 @@
+using System.Collections;
 using UnityEngine;
 
 public class FollowEnemy : MonoBehaviour
 {
-  public float moveSpeed;
-  public Transform target;
-  public float sightRange;
-  public float chaseRange;
-  public bool canChase;
-
-  private Vector2 direction;
-  private Rigidbody2D _rigidbody2D;
-  private Vector2 _X, _Y;
-  
-  [SerializeField] private LayerMask whatIsPlayer;
-  
-  private void Awake()
-  {
-    _rigidbody2D = GetComponent<Rigidbody2D>();
-  }
-  private void Start()
-  {
-    target = GameObject.Find("Player").transform;
-  }
-  private void Update()
-  {
-    if (target == null) return;
+    public float enemyHealth;
     
-    SetDirectionDistance();
-    _X = new Vector2(Mathf.Sign(direction.x) * moveSpeed, 0);
-    _Y = new Vector2(0, Mathf.Abs(direction.y));
+    //Movement related
+    public float moveSpeed;
+    public Vector2 direction;
+    private Rigidbody2D _rigidbody;
+    public Transform target;
+    private Vector2 _X, _Y;
+    
+    //dmg related
+    private float _damageCooldownTimer = 1f;
+    public bool isDamaged;
+    public ParticleSystem BloodFX;
+    
+    // chase
+    public bool canChase;
+    public float chaseRange;
+    
+    private PlayerController _player;
+    [SerializeField] private LayerMask whatIsPlayer;
 
-    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+    
+    private void Start()
     {
-      _rigidbody2D.linearVelocity = _X;
-    }
-    else
-    {
-      _rigidbody2D.linearVelocity = _Y;
-    }
-
-    if (Vector2.Distance(target.position, transform.position) < sightRange)
-    {
-      canChase = true;
-    }
-    else if (Vector2.Distance(target.position, transform.position) > chaseRange)
-    {
-      canChase = false;
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _player = FindFirstObjectByType<PlayerController>();
+        isDamaged = false;
     }
     
-  }
-  private void SetDirectionDistance()
-  {
-    direction = (Vector2)target.position - (Vector2)transform.position;
-    direction = new Vector2(Mathf.Round(direction.x), Mathf.Round(direction.y));
-  }
+    private void Update()
+    {
+        //Chases the player if the bool is true
+        if (canChase && !isDamaged)
+        {
+            SetDirectionDistance();
+        }
+        
+        _X = new Vector2(Mathf.Sign(direction.x) * moveSpeed, 0);
+        _Y = new Vector2(0, Mathf.Sign(direction.y) * moveSpeed);
+        
 
+        //Sets if the rangeEnemy can chase the player or not
+        if (Vector2.Distance(target.position, transform.position) < chaseRange)
+        {
+            canChase = true;
+        }
+        if (Vector2.Distance(target.position, transform.position) > chaseRange)
+        {
+            canChase = false;
+            _rigidbody.linearVelocityX = 0;
+            _rigidbody.linearVelocityY = 0;
+        }
+
+        
+    }
+    
+    private void SetDirection(Vector2 direction)
+    {
+        _rigidbody.linearVelocity = direction;
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        RotationPoint.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
+    //Moves towards the player
+    private void SetDirectionDistance()
+    {
+        direction = (Vector2)target.position - (Vector2)transform.position;
+        direction = new Vector2(Mathf.Round(direction.x), Mathf.Round(direction.y));
+    }
+
+    //Takes damage
+    private IEnumerator TakeDamage()
+    {
+        if (Time.time > _damageCooldownTimer)
+        {
+            isDamaged = true;
+            enemyHealth--;
+            _damageCooldownTimer = Time.time + 1f;
+            BloodFX.Play();
+            
+            yield return new WaitForSeconds(1f);
+            isDamaged = false;
+            
+            //Destroys enemy on death
+            if (enemyHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+        
 }
