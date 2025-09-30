4d63143408483afbe1bbabeef52afe2caa0fbfaf
westenravn-boop <westenravn@gmail.com>
2025-09-30 10:04:50 +0200

    Update FollowEnemy

--- Assets/Scripts/Enemy/FollowEnemy.cs
+++ Assets/Scripts/Enemy/FollowEnemy.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class FollowEnemy : MonoBehaviour
 {
   public float moveSpeed;
-
   public Transform target;
-  
-  private Vector2 _moveDirection;
-  private Rigidbody2D _rigidbody2D;
 
+  private Vector2 direction;
+  private Rigidbody2D _rigidbody2D;
+  private Vector2 _X, _Y;
+  
+  [SerializeField] private LayerMask whatIsPlayer;
+  
   private void Awake()
   {
     _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -17,21 +19,26 @@ public class FollowEnemy : MonoBehaviour
   {
     target = GameObject.Find("Player").transform;
   }
-  
-  }
-  private void
-    OnTriggerEnter2D(Collider2D other)
+  private void Update()
   {
-    if (other.CompareTag("AttackHitbox"))
+    SetDirectionDistance();
+    _X = new Vector2(Mathf.Sign(direction.x) * moveSpeed, 0);
+    _Y = new Vector2(0, Mathf.Abs(direction.y))
+
+    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
     {
-      Destroy(gameObject);
+      _rigidbody2D.linearVelocity = _X;
+    }
+    else
+    {
+      _rigidbody2D.linearVelocity = _Y;
     }
   }
-  private void FixedUpdate()
+
+  private void SetDirectionDistance()
   {
-    if (target)
-    {
-      _rigidbody2D.linearVelocity = new Vector2(_moveDirection.x, _moveDirection.y) * moveSpeed;
-    }
+    direction = (Vector2)PlayerMovement.Player.position - (Vector2)transform.position;
+    Mathf.Round(direction.x), Mathf.Round(direction.y);
   }
+
 }
