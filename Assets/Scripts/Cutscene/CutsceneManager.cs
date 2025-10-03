34930db33f0a636ab90e0602ab38912ba297a733
e1ayas <elias.mutu06@gmail.com>
2025-10-03 09:57:25 +0200

    Fixes

--- Assets/Scripts/Cutscene/CutsceneManager.cs
+++ Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -2,8 +2,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CutsceneManager : MonoBehaviour
 {
-    
+
+    public float startTime = 25f;
+    public float currentTime;
+    private bool timerRunning = false;
+
+
+    private void Start()
+    {
+        currentTime = startTime;
+        timerRunning = true;
+
+    }
+
+    private void Update()
+    {
+        if (timerRunning)
+        {
+            if (currentTime > 0)
+            {
+                currentTime -= Time.deltaTime;
+            }
+            else
+            {
+                {
+                    currentTime = 0;
+                    timerRunning = false;
+                }
+            }
+        }
+
+        if (timerRunning == false)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
 }
